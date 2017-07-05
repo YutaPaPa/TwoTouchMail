73c464df01ed2455e66b471b0013207d3c84fb80
YutaPaPa <49ers.Champ@gmail.com>
2017-07-06 00:29:36 +0900

    完成

--- app/src/main/java/com/example/masayuki/twotouchmail/MainActivity.java
+++ app/src/main/java/com/example/masayuki/twotouchmail/MainActivity.java
@@ -11,50 +11,68 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.telephony.SmsManager;
+import android.app.AlertDialog;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button btnSend = (Button) this.findViewById(R.id.button);
+        btnSend.setOnClickListener(this);
+    }
 
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RadioGroup rgMailTo = (RadioGroup) findViewById(R.id.rg_mailTo);
-                int checkedId = rgMailTo.getCheckedRadioButtonId();
-                String mailTo = ((RadioButton) findViewById(checkedId)).getText().toString();
-                //int intMailTo = ((RadioButton) findViewById(checkedId)).getId();
+    @Override
+    public void onClick(View v) {
+        //EditText editText = (EditText) this.findViewById(R.id.editText);
+        RadioGroup rgMailTo = (RadioGroup) findViewById(R.id.rg_mailTo);
+        int checkedId = rgMailTo.getCheckedRadioButtonId();
+        String mailTo = ((RadioButton) findViewById(checkedId)).getText().toString();
 
-                Log.d("mailTo:", mailTo);
-                Log.d("checkedId:", "checkedId:" + checkedId);
+        Resources res = getResources();
+        Uri uri = null;
+        switch (mailTo) {
+            case "ドコモ":
+                uri = Uri.parse("mailto:" + res.getString(R.string.mail_to_docomo).toString());
+                break;
+            case "Yahoo":
+                uri = Uri.parse("mailto:" + res.getString(R.string.mail_to_yahoo).toString());
+                break;
+            case "Gmail":
+                uri = Uri.parse("mailto:" + res.getString(R.string.mail_to_yahoo).toString());
+                break;
+            case "悠太":
+                uri = Uri.parse("sms:" + res.getString(R.string.mail_to_yuta).toString());
 
-                //EditText edit01 = (EditText) findViewById(R.id.editText);
-                //String title = edit01.getText().toString();
-
-                Resources res = getResources();
-                Uri uri = null;
-                switch (mailTo)
+                /*if (editText.getText().toString().equals(""))
                 {
-                    case "ドコモ":
-                        uri = Uri.parse("mailto:" + res.getString(R.string.mail_to_docomo).toString());
-                        break;
-                    case "Yahoo":
-                        uri = Uri.parse("mailto:" + res.getString(R.string.mail_to_yahoo).toString());
-                        break;
-                    case "Gmail":
-                        uri = Uri.parse("mailto:" + res.getString(R.string.mail_to_yahoo).toString());
-                        break;
-                }
+                    AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                    alert.setTitle(res.getString(R.string.error_title_msg).toString());
+                    alert.setMessage(res.getString(R.string.not_input_msg).toString());
+                    alert.setIcon(R.drawable.error);
+                    alert.setPositiveButton("OK", null);
+                    alert.show();
+                }*/
+                /*else
+                {
+                    SmsManager smsManager = SmsManager.getDefault();
 
-                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
-                intent.putExtra(Intent.EXTRA_SUBJECT, "");
-                intent.putExtra(Intent.EXTRA_TEXT, "");
-                startActivity(intent);
+                    String destinationAddress = res.getString(R.string.mail_to_yuta).toString();
+                    String scAddress = null;
+                    String text = editText.getText().toString();
+                    smsManager.sendTextMessage(destinationAddress, scAddress, text, null, null);
+                }*/
+                break;
+        }
 
-            }
-        });
+        //if (mailTo.equals(res.getString(R.string.docomo).toString()) || mailTo.equals(res.getString(R.string.yahoo).toString()) || mailTo.equals(res.getString(R.string.gmail).toString())) {
+            Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+            intent.putExtra(Intent.EXTRA_SUBJECT, "");
+            intent.putExtra(Intent.EXTRA_TEXT, "");
+            startActivity(intent);
+        //}
     }
 }
